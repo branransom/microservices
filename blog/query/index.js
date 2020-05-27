f6cea3f6dc885ceaaf9c381e0aaa016b82e06a7e
branransom <branransom@gmail.com>
2020-05-27 11:01:35 -0400

    Dockerize all microservices

--- blog/query/index.js
+++ blog/query/index.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const axios = require("axios");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -9,25 +9,25 @@ app.use(cors());
 
 const posts = {};
 
-app.get("/posts", (req, res) => {
+app.get('/posts', (req, res) => {
   res.send(posts);
 });
 
 const handleEvent = (type, data) => {
-  if (type === "PostCreated") {
+  if (type === 'PostCreated') {
     const { id, title } = data;
 
     posts[id] = { id, title, comments: [] };
   }
 
-  if (type === "CommentCreated") {
+  if (type === 'CommentCreated') {
     const { id, content, postId, status } = data;
 
     const post = posts[postId];
     post.comments.push({ id, content, status });
   }
 
-  if (type === "CommentUpdated") {
+  if (type === 'CommentUpdated') {
     const { id, content, postId, status } = data;
 
     const post = posts[postId];
@@ -38,7 +38,7 @@ const handleEvent = (type, data) => {
   }
 };
 
-app.post("/events", (req, res) => {
+app.post('/events', (req, res) => {
   const { type, data } = req.body;
 
   handleEvent(type, data);
@@ -47,12 +47,12 @@ app.post("/events", (req, res) => {
 });
 
 app.listen(4002, async () => {
-  console.log("Listening on 4002");
+  console.log('Listening on 4002');
 
-  const res = await axios.get("http://localhost:4005/events");
+  const res = await axios.get('http://event-bus-srv:4005/events');
 
   for (let event of res.data) {
-    console.log("Processing event:", event.type);
+    console.log('Processing event:', event.type);
 
     handleEvent(event.type, event.data);
   }
