c942cf5cf50d3846e29817a5c0ed10e25485ea3e
Brandon Ransom <Brandon.Ransom@LibertyMutual.com>
2020-09-14 21:34:36 -0400

    Add nats streaming server, and implement basic pub/sub

---
+++ ticketing/nats-test/src/listener.ts
@@ -0,0 +1,26 @@
+import nats, { Message } from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
+
+console.clear();
+
+const client = nats.connect('ticketing', randomBytes(4).toString('hex'), {
+  url: 'http://localhost:4222',
+});
+
+client.on('connect', () => {
+  console.log('Listener connected to nats');
+
+  const options = client.subscriptionOptions().setManualAckMode(true);
+
+  const subscription = client.subscribe('ticket:created', 'orders-service-queue-group', options);
+
+  subscription.on('message', (msg: Message) => {
+    const data = msg.getData();
+
+    if (typeof data === 'string') {
+      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+    }
+
+    msg.ack();
+  });
+});
